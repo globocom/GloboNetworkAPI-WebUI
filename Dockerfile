63a4d3e7cbfba33bb034ff4e64d9146b16e46349
henriquebonadio <henrique.bonadio@corp.globo.com>
2017-08-30 10:45:10 -0300

    docker for netapi_webui

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:2.7
+
+RUN mkdir -p /netapi_webui
+WORKDIR /netapi_webui
+
+ADD . /netapi_webui/
+
+CMD cd /netapi_webui
+
+EXPOSE 8080
+
+RUN apt-get update
+RUN apt-get install libldap2-dev libsasl2-dev libssl-dev -y
+RUN apt-get install python-ldap -y
+RUN apt-get install net-tools dnsutils -y
+
+RUN pip install -r requirements.txt
+#RUN pip install -r requirements_test.txt
+RUN pip install virtualenv
+RUN pip install gunicorn
---
+++ docker-start.sh
@@ -0,0 +1,33 @@
+# Waits for other containers availability
+sleep 5
+
+# Resolves the networkapi container IP and sets it on the /etc/hosts
+NETAPI_IP=$(nslookup netapi_app | grep Address | tail -1 | awk '{print $2}')
+printf "$NETAPI_IP\tnetworkapi.docker\n" >> /etc/hosts
+
+echo -e "PYTHONPATH=\"/netapi_webui/networkapi_webui:/netapi_webui/$PYTHONPATH\"" >> /etc/environment
+
+cat > /etc/init.d/gunicorn_networkapi_webui <<- EOM
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          scriptname
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
+/usr/local/bin/gunicorn -c /netapi_webui/gunicorn.conf.py cadvlan_wsgi:application
+EOM
+
+chmod 777 /etc/init.d/gunicorn_networkapi_webui
+update-rc.d gunicorn_networkapi_webui defaults
+export PYTHONPATH="/netapi_webui/networkapi_webui:/netapi_webui/$PYTHONPATH"
+
+echo "starting gunicorn"
+/etc/init.d/gunicorn_networkapi_webui start
+
+touch /tmp/gunicorn-globonetworkapi_webui_error.log
+tail -f /tmp/gunicorn-globonetworkapi_webui_error.log
