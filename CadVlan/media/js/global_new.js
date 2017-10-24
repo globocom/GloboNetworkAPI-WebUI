4aa2a7a5db58271afdc850bd6340719d534e0ece
juanaugusto <juan.augusto@ufrj.br>
2017-10-24 14:40:17 -0200

    When filter pools in new pool list, send ajax only when interval between key presses is better than 250ms

--- CadVlan/media/js/global_new.js
+++ CadVlan/media/js/global_new.js
@@ -8,3 +8,23 @@ $(document).ready(function() {
         $(".loading").hide();
     });
 });
+
+function debounce(fn, delay) {
+    var timer = null;
+    return function () {
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn.apply(context, args);
+        }, delay);
+    };
+}
+
+function checkProhibitedKeys(pressedKey, prohibitedKeys){
+    for (var i = 0, len = prohibitedKeys.length; i < len; i++) {
+        if(pressedKey == prohibitedKeys[i]){
+            return false;
+        }
+    }
+    return true;
+}
