5cb78e7a254bfd214f4382ec97a233336512fd97
laurapanzariello <laura.panzariello@corp.globo.com>
2018-05-21 11:01:00 -0300

    use bootstrap forms style

--- CadVlan/settings.py
+++ CadVlan/settings.py
@@ -74,6 +74,10 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH, 'media')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = "/media"
 
+STATIC_DIRS = (
+    os.path.join(PROJECT_ROOT_PATH, 'static')
+)
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
