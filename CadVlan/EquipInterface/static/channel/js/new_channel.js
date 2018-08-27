9163f34d56e8b97dd0b7541859b259ba10e6ee56
laura.panzariello <laura.panzariello@corp.globo.com>
2018-08-27 11:19:09 -0300

    If is a trunk interface, add and remove environments dynamically

--- CadVlan/EquipInterface/static/channel/js/new_channel.js
+++ CadVlan/EquipInterface/static/channel/js/new_channel.js
@@ -1,4 +1,5 @@
 var total_sw = 1;
+var total_env = 1;
 
 $(document).ready(function() {
 
@@ -27,7 +28,7 @@ $(document).ready(function() {
         $(inputEquip).attr('required', 'required');
         $(inputEquip).attr('type','text');
         $(inputEquip).attr('name','switchname');
-        $(inputEquip).attr('placeholder','Busque pelo nome');
+        $(inputEquip).attr('placeholder','Busque pelo nome...');
         $(inputEquip).appendTo($(addBlockElemSw));
 
         var addBlockElemInt = document.createElement('div');
@@ -72,4 +73,93 @@ $(document).ready(function() {
 
         $(addBlockRow).appendTo($('#sw_content'));
     });
+
+    $('#btn_channel_env').click(function() {
+
+        var addBlockId = total_env = total_env + 1;
+
+        var addBlockRow = document.createElement('div');
+        $(addBlockRow).addClass('form-row-cadvlan');
+        $(addBlockRow).attr('id','form_env_vlans_' + addBlockId);
+
+        var addBlockElemSw = document.createElement('div');
+        $(addBlockElemSw).addClass('form-group');
+        $(addBlockElemSw).appendTo($(addBlockRow));
+
+        var labelEquip = document.createElement('Label');
+        $(labelEquip).addClass('form-control-label');
+        $(labelEquip).attr('for', 'channel_number' + addBlockId);
+        labelEquip.innerHTML = "Ambiente";
+        $(labelEquip).appendTo($(addBlockElemSw));
+
+        var inputEquip = document.createElement('input');
+        $(inputEquip).addClass('form-control');
+        $(inputEquip).attr('type','text');
+        $(inputEquip).attr('name','environment');
+        $(inputEquip).attr('placeholder','Busque pelo nome...');
+        $(inputEquip).appendTo($(addBlockElemSw));
+
+        var addBlockElemInt = document.createElement('div');
+        $(addBlockElemInt).addClass('form-group');
+        $(addBlockElemInt).appendTo($(addBlockRow));
+
+        var labelInt = document.createElement('Label');
+        $(labelInt).addClass('form-control-label');
+        $(labelInt).attr('for', 'rangevlans' + addBlockId);
+        labelInt.innerHTML = "Interface";
+        $(labelInt).appendTo($(addBlockElemInt));
+
+        var inputInt = document.createElement('input');
+        $(inputInt).addClass('form-control');
+        $(inputInt).attr('type','text');
+        $(inputInt).attr('name','rangevlans' + addBlockId);
+        $(inputInt).attr('placeholder','Ex.: 1-10');
+        $(inputInt).appendTo($(addBlockElemInt));
+
+        var addBlockButton = document.createElement('button');
+        $(addBlockButton).addClass('btn btn-social-bottom btn-responsive channel');
+        $(addBlockButton).attr('type', 'button');
+        $(addBlockButton).attr('id','btn_remove_envs' + addBlockId);
+        $(addBlockButton).attr('name', addBlockId);
+        $(addBlockButton).attr('data-toggle', 'tooltip');
+        $(addBlockButton).attr('title','Remover este ambiente.');
+        addBlockButton.addEventListener( 'click', function(){
+            var Ids = this.name;
+            var elementId = "form_env_vlans_".concat(Ids);
+            var node = document.getElementById(elementId);
+            if (node.parentNode) {
+                node.parentNode.removeChild(node);
+            }
+        } );
+        $(addBlockButton).appendTo($(addBlockRow));
+
+        var addBlockI = document.createElement('i');
+        $(addBlockI).addClass('material-icons');
+        $(addBlockI).attr('style', 'color:#FFD17C;font-size:15px;left:50%;');
+        addBlockI.innerHTML = "delete";
+        $(addBlockI).appendTo($(addBlockButton));
+
+        $(addBlockRow).appendTo($('#more_envs'));
+    });
 });
+
+function interfaceTrunk() {
+
+    var checkBox = document.getElementById("trunk");
+    var envsDiv = document.getElementById("more_envs");
+
+    if (checkBox.checked == true){
+        envsDiv.style.display = "flex";
+        envsDiv.style.flexDirection = "column";
+    }
+}
+
+function interfaceAccess() {
+
+    var checkBox = document.getElementById("access");
+    var envsDiv = document.getElementById("more_envs");
+
+    if (checkBox.checked == true){
+        envsDiv.style.display = "none";
+    }
+}
