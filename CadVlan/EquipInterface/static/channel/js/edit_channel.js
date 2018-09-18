60902d238aa79c6637c1eaeaf795fec5917ea2fb
laura.panzariello <laura.panzariello@corp.globo.com>
2018-09-18 18:22:13 -0300

    choose envs to server interface when trunk

--- CadVlan/EquipInterface/static/channel/js/edit_channel.js
+++ CadVlan/EquipInterface/static/channel/js/edit_channel.js
@@ -1,4 +1,5 @@
 var total_env = 1;
+var totalServer = 1;
 
 $(document).ready(function() {
 
@@ -29,6 +30,7 @@ $(document).ready(function() {
 	}});
 
     fillEnvironmentField( "#envs", "environment_list", "rangevlans");
+    fillEnvironmentField( "#ch_envs", "environment_list", "ch_rangevlans");
 
     fillInterfaceField("#form_switch_name", "equipment_list", "form_switch_interface");
 
@@ -118,10 +120,19 @@ $(document).ready(function() {
         fillEnvironmentField( autocompleteId, "environment_list", vlanId);
 
     });
+
 });
 
 function removeEnvs(counter) {
     let elementId = "env_row".concat(counter);
+    let node = document.getElementById(elementId);
+    if (node.parentNode) {
+        node.parentNode.removeChild(node);
+    }
+}
+
+function removeEnvsCh(counter) {
+    let elementId = "ch_env_row".concat(counter);
     let node = document.getElementById(elementId);
     if (node.parentNode) {
         node.parentNode.removeChild(node);
@@ -202,4 +213,105 @@ function channelTrunk(interface_id) {
     } else {
         envsDiv.style.display = "none";
     }
+}
+
+function serverTrunk(interface_id, type_id) {
+    let serverName = "server".concat(interface_id),
+        trunkName = serverName.concat('trunk')
+        trunk_id = trunkName.concat(type_id),
+        divId = "more_envs_ch".concat(interface_id)
+        envsDiv = document.getElementById(divId)
+        checkBoxTrunk = document.getElementById(trunk_id);
+
+    if (checkBoxTrunk.checked == true){
+        envsDiv.style.display = "flex";
+        envsDiv.style.flexDirection = "column";
+        let ids = interface_id + "_div_1"
+        let ch_envId = "#ch_envs".concat(ids);
+        let ch_rangID = "ch_rangevlans".concat(ids);
+        fillEnvironmentField( ch_envId, "environment_list", ch_rangID);
+
+    } else {
+        envsDiv.style.display = "none";
+    }
+}
+
+function moreEnvsCh(interface_id, foorloopNumber) {
+
+        totalServer = totalServer + 1 + foorloopNumber;
+        let idCounter = interface_id + "_div_" + totalServer;
+        let addBlockRow = document.createElement('div');
+        $(addBlockRow).addClass('rows');
+        $(addBlockRow).attr('id','ch_form_env_vlans_' + idCounter);
+
+        let addBlockElemSw = document.createElement('div');
+        $(addBlockElemSw).addClass('text-fields');
+        $(addBlockElemSw).appendTo($(addBlockRow));
+
+        let labelEquip = document.createElement('Label');
+        $(labelEquip).addClass('form-control-label');
+        $(labelEquip).attr('for', 'ch_envs' + idCounter);
+        labelEquip.innerHTML = "Ambiente";
+        $(labelEquip).appendTo($(addBlockElemSw));
+
+        let inputEquip = document.createElement('input');
+        $(inputEquip).addClass('form-control bs-autocomplete');
+        $(inputEquip).attr('autocomplete','off');
+        $(inputEquip).attr('type','text');
+        $(inputEquip).attr('id','ch_envs' + idCounter);
+        $(inputEquip).attr('name','environmentCh' + interface_id);
+        $(inputEquip).attr('placeholder','Busque pelo nome...');
+        $(inputEquip).appendTo($(addBlockElemSw));
+
+        let addBlockElemInt = document.createElement('div');
+        $(addBlockElemInt).addClass('text-fields');
+        $(addBlockElemInt).appendTo($(addBlockRow));
+
+        let labelInt = document.createElement('Label');
+        $(labelInt).addClass('form-control-label');
+        $(labelInt).attr('for', 'ch_rangevlans' + idCounter);
+        labelInt.innerHTML = "Range de Vlan";
+        $(labelInt).appendTo($(addBlockElemInt));
+
+        let inputInt = document.createElement('input');
+        $(inputInt).addClass('form-control');
+        $(inputInt).attr('type','text');
+        $(inputInt).attr('id','ch_rangevlans' + idCounter);
+        $(inputInt).attr('name','rangevlanCh' + interface_id);
+        $(inputInt).attr('placeholder','Ex.: 1-10');
+        $(inputInt).appendTo($(addBlockElemInt));
+
+        let addBlockElemButton = document.createElement('div');
+        $(addBlockElemButton).addClass('text-fields');
+        $(addBlockElemButton).appendTo($(addBlockRow));
+
+        let addBlockButton = document.createElement('button');
+        $(addBlockButton).addClass('btn btn-social-bottom btn-responsive channel');
+        $(addBlockButton).attr('type', 'button');
+        $(addBlockButton).attr('id','btn_remove_envs_ch' + idCounter);
+        $(addBlockButton).attr('name', interface_id);
+        $(addBlockButton).attr('data-toggle', 'tooltip');
+        $(addBlockButton).attr('title','Remover este ambiente.');
+        addBlockButton.addEventListener( 'click', function(){
+            let Ids = this.name;
+            let elementId = "ch_form_env_vlans_".concat(idCounter);
+            let node = document.getElementById(elementId);
+            if (node.parentNode) {
+                node.parentNode.removeChild(node);
+            }
+        } );
+        $(addBlockButton).appendTo($(addBlockElemButton));
+
+        let addBlockI = document.createElement('i');
+        $(addBlockI).addClass('material-icons');
+        $(addBlockI).attr('style', 'color:#FFD17C;font-size:15px;left:50%;');
+        addBlockI.innerHTML = "delete";
+        $(addBlockI).appendTo($(addBlockButton));
+
+        let divEnvId = "#more_envs_ch".concat(interface_id);
+        $(addBlockRow).appendTo($(divEnvId));
+
+        let autocompleteId = '#ch_envs'.concat(idCounter);
+        let vlanId = 'ch_rangevlans'.concat(idCounter);
+        fillEnvironmentField( autocompleteId, "environment_list", vlanId);
 }
