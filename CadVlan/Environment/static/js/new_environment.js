9921acba8523c3e9347510180e739eafe1b16821
laura.panzariello <laura.panzariello@corp.globo.com>
2019-06-19 15:06:36 -0300

    refactor create a new environment

--- CadVlan/Environment/static/js/new_environment.js
+++ CadVlan/Environment/static/js/new_environment.js
@@ -1,3 +1,5 @@
+var total_env = 1;
+
 $(document).ready(function() {
 
     $('[data-toggle="tooltip"]').tooltip();
@@ -17,23 +19,81 @@ $(document).ready(function() {
     autocompleteAjax ("/autocomplete/vrf/", "vrf_list")
     fillEnvironmentField("vrf", "vrf_list");
 
+    $('#more_range').click(function() {
+
+        let addBlockId = total_env = total_env + 1;
+
+        let addBlockRow = document.createElement('div');
+        $(addBlockRow).addClass('form-row-cadvlan');
+        $(addBlockRow).attr('id','rangevlans_' + addBlockId);
+
+        let addBlockElemSw = document.createElement('div');
+        $(addBlockElemSw).addClass('form-group');
+        $(addBlockElemSw).appendTo($(addBlockRow));
+
+        let addBlockElemField = document.createElement('div');
+        $(addBlockElemField).addClass('field-group');
+        $(addBlockElemField).appendTo($(addBlockElemSw));
+
+        let addBlockElemButton = document.createElement('div');
+        $(addBlockElemButton).addClass('field-group');
+        $(addBlockElemButton).appendTo($(addBlockElemSw));
+
+        let labelInt = document.createElement('Label');
+        $(labelInt).addClass('form-control-label');
+        $(labelInt).attr('for', 'vlan_range_' + addBlockId);
+        labelInt.innerHTML = "Range de Vlan " + addBlockId;
+        $(labelInt).appendTo($(addBlockElemField));
+
+        let inputInt = document.createElement('input');
+        $(inputInt).addClass('form-control');
+        $(inputInt).attr('type','text');
+        $(inputInt).attr('id','vlan_range_' + addBlockId);
+        $(inputInt).attr('name','vlan_range');
+        $(inputInt).attr('placeholder','MIN-MAX');
+        $(inputInt).appendTo($(addBlockElemField));
+
+        let addBlockButton = document.createElement('button');
+        $(addBlockButton).addClass('btn btn-social-bottom btn-responsive channel');
+        $(addBlockButton).attr('type', 'button');
+        $(addBlockButton).attr('style', 'margin-bottom: 0px;');
+        $(addBlockButton).attr('id','btn_remove_envs' + addBlockId);
+        $(addBlockButton).attr('name', addBlockId);
+        $(addBlockButton).attr('data-toggle', 'tooltip');
+        $(addBlockButton).attr('title','Remover.');
+        addBlockButton.addEventListener( 'click', function(){
+            let Ids = this.name;
+            let elementId = "rangevlans_".concat(Ids);
+            let node = document.getElementById(elementId);
+            if (node.parentNode) {
+                node.parentNode.removeChild(node);
+            }
+        } );
+        $(addBlockButton).appendTo($(addBlockElemButton));
+
+        let addBlockI = document.createElement('i');
+        $(addBlockI).addClass('material-icons');
+        $(addBlockI).attr('style', 'color:#FFD17C;font-size:15px;left:50%;');
+        addBlockI.innerHTML = "delete";
+        $(addBlockI).appendTo($(addBlockButton));
+
+        $(addBlockRow).appendTo($('#div_more_range'));
+
+    });
+
 });
 
 function fillEnvironmentField(envFieldId, storageName) {
 
     let jsonEnv = JSON.parse(localStorage.getItem(storageName));
-    console.log(jsonEnv)
-    console.log(storageName)
     let envField = document.getElementById(envFieldId);
 
     $.each(jsonEnv, function(){
-        $(envField).append($("<option />").val(this).text(this));
+        $(envField).append($("<option />").val(this.id).text(this.name));
     })
-
-    console.log("oi")
 }
 
-function autocompleteAjax (url, object) {
+function autocompleteAjax(url, object) {
     $.ajax({
         url: url,
         dataType: "json",
@@ -44,4 +104,17 @@ function autocompleteAjax (url, object) {
                 localStorage.setItem(object, JSON.stringify(data.list));
             }
 	}});
+}
+
+function envACL() {
+
+    let checkBox = document.getElementById("acl_option");
+    let envsDiv = document.getElementById("acl_info");
+
+    if (checkBox.checked == true){
+        envsDiv.style.display = "flex";
+        envsDiv.style.flexDirection = "column";
+    } else {
+        envsDiv.style.display = "none";
+    }
 }
--- CadVlan/Environment/views_v3.py
+++ CadVlan/Environment/views_v3.py
@@ -17,17 +17,19 @@
 import logging
 
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.http import HttpResponseRedirect
+
 from networkapiclient.exception import NetworkAPIClientError
 
 from CadVlan.Auth.AuthSession import AuthSession
 from CadVlan.Environment.business import cache_environment_list
-
+from CadVlan.messages import environment_messages
 from CadVlan.templates import ADD_ENVIRONMENT
 from CadVlan.templates import AJAX_AUTOCOMPLETE_ENVIRONMENT
 from CadVlan.templates import AJAX_AUTOCOMPLETE_VLAN_ENVIRONMENT
-
 from CadVlan.Util.Decorators import log
 from CadVlan.Util.Decorators import login_required
 from CadVlan.Util.shortcuts import render_to_response_ajax
@@ -87,7 +89,7 @@ def ajax_autocomplete_environment(request):
             "extends_search": []
         }
 
-        envs = client.create_api_environment().search(fields=["name"], search=data)
+        envs = client.create_api_environment().search(fields=["id", "name"], search=data)
         env_list = cache_environment_list(envs.get('environments'))
 
     except NetworkAPIClientError, e:
@@ -191,8 +193,28 @@ def ajax_autocomplete_environment_logic(request):
 @login_required
 def add_environment(request):
 
-    # auth = AuthSession(request.session)
-    # client = auth.get_clientFactory()
+    auth = AuthSession(request.session)
+    client = auth.get_clientFactory()
     lists = list()
 
+    if request.method == 'POST':
+        vlan_range = request.POST.get('vlan_range').split('-')
+        env = {
+            "grupo_l3": int(request.POST.get('fisic_env')),
+            "ambiente_logico": int(request.POST.get('logic_env')),
+            "divisao_dc": int(request.POST.get('router_env')),
+            "min_num_vlan_1": int(vlan_range[0]),
+            "max_num_vlan_1": int(vlan_range[1]),
+            "min_num_vlan_2": int(vlan_range[0]),
+            "max_num_vlan_2": int(vlan_range[1]),
+            "default_vrf": int(request.POST.get('vrf')),
+            "father_environment": int(request.POST.get('father_env')) if request.POST.get('father_env') else None,
+            'vxlan': True if request.POST.get('vxlan') else False
+        }
+        client.create_api_environment().create([env])
+        messages.add_message(request, messages.SUCCESS, environment_messages.get("success_insert"))
+
+        return HttpResponseRedirect(reverse("environment.list"))
+
     return render_to_response(ADD_ENVIRONMENT, lists, RequestContext(request))
+
