60929d732efb22675d297bd5f0816115bdb9ea2c
laura.panzariello <laura.panzariello@corp.globo.com>
2019-09-24 14:30:41 -0300

    update the create v4 page

--- CadVlan/Net/forms.py
+++ CadVlan/Net/forms.py
@@ -13,38 +13,81 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from django import forms
 
+from django import forms
 from CadVlan.messages import error_messages
 
 NETWORK_IP_CHOICES = ((0, "IPv4"), (1, "IPv6"))
 
 
 class IPForm(forms.Form):
-    equip_name = forms.CharField(label=u'Nome do Equipamento', min_length=3, required=True, widget=forms.TextInput(
-        attrs={'style': "width: 300px; height: 19px;", 'class': "ui-state-default", 'autocomplete': "off"}), error_messages=error_messages)
-    descricao = forms.CharField(label=u'Descrição', min_length=3, max_length=100, required=False, error_messages=error_messages, widget=forms.Textarea(
-        attrs={'style': "width: 500px", 'rows': 3, 'data-maxlenght': 100}))
+    """Form to create an Ip
+
+    """
+
+    equip_name = forms.CharField(label="Nome do Equipamento",
+                                 min_length=3,
+                                 required=True,
+                                 widget=forms.TextInput(attrs={'style': "width: 400px; ",
+                                                               'class': "ui-state-default",
+                                                               'autocomplete': "off"}),
+                                 error_messages=error_messages)
+
+    descricao = forms.CharField(label="Descrição",
+                                min_length=3,
+                                max_length=100,
+                                required=False,
+                                error_messages=error_messages,
+                                widget=forms.Textarea(attrs={'style': "width: 400px; height: 19px;",
+                                                             'data-maxlenght': 100}))
 
 
 class IPEditForm(forms.Form):
-    descricao = forms.CharField(label=u'Descrição', min_length=3, max_length=100, required=False, error_messages=error_messages, widget=forms.Textarea(
-        attrs={'style': "width: 500px", 'rows': 3, 'data-maxlenght': 100}))
-    equip_names = forms.CharField(
-        label=u'', required=False, widget=forms.HiddenInput())
+    """Form to edit an association between an Ip and a network
+
+    """
+
+    descricao = forms.CharField(label=u'Descrição',
+                                min_length=3,
+                                max_length=100,
+                                required=False,
+                                error_messages=error_messages,
+                                widget=forms.Textarea(attrs={'style': "width: 500px",
+                                                             'rows': 3,
+                                                             'data-maxlenght': 100}))
+
+    equip_names = forms.CharField(label=u'',
+                                  required=False,
+                                  widget=forms.HiddenInput())
 
 
 class IPAssocForm(forms.Form):
-    equip_name = forms.CharField(label=u'Nome do Equipamento', min_length=3, required=True, widget=forms.TextInput(
-        attrs={'style': "width: 300px; height: 19px;", 'class': "ui-state-default", 'autocomplete': "off"}), error_messages=error_messages)
-    descricao = forms.CharField(label=u'Descrição', min_length=3, max_length=100, required=False, error_messages=error_messages, widget=forms.Textarea(
-        attrs={'style': "width: 500px", 'rows': 3, 'data-maxlenght': 100}))
-    equip_names = forms.CharField(
-        label=u'', required=False, widget=forms.HiddenInput())
+    """Form to associate an Ip to a network
+
+    """
+
+    equip_name = forms.CharField(label=u'Nome do Equipamento',
+                                 min_length=3,
+                                 required=True,
+                                 widget=forms.TextInput(attrs={'style': "width: 300px; height: 19px;",
+                                                               'class': "ui-state-default", 'autocomplete': "off"}),
+                                 error_messages=error_messages)
+    descricao = forms.CharField(label=u'Descrição',
+                                min_length=3,
+                                max_length=100,
+                                required=False,
+                                error_messages=error_messages, widget=forms.Textarea(attrs={'style': "width: 500px",
+                                                                                            'rows': 3,
+                                                                                            'data-maxlenght': 100}))
+    equip_names = forms.CharField(label=u'',
+                                  required=False,
+                                  widget=forms.HiddenInput())
 
 
 class NetworkForm(forms.Form):
+    """Form to create a network
 
+    """
     def __init__(self, net_type_list, env_vip_list, *args, **kwargs):
         super(NetworkForm, self).__init__(*args, **kwargs)
 
@@ -59,34 +102,60 @@ class NetworkForm(forms.Form):
         self.fields['net_type'].choices = net_choices
         self.fields['env_vip'].choices = env_choices
 
-    vlan_name = forms.CharField(label=u'Número da Vlan', min_length=3, max_length=100, required=True, widget=forms.TextInput(
-        attrs={'style': "width: 300px; height: 19px;", 'class': "ui-state-default", 'autocomplete': "off"}), error_messages=error_messages)
-    vlan_name_id = forms.IntegerField(
-        label="Id da Vlan", required=True, error_messages=error_messages, widget=forms.HiddenInput())
-    ip_version = forms.ChoiceField(label="Rede IPv4/IPv6", required=True, choices=NETWORK_IP_CHOICES,
-                                   error_messages=error_messages, widget=forms.RadioSelect, initial=0)
-    networkv4 = forms.CharField(label="Rede IPv4", required=False, min_length=1,
-                                max_length=18, error_messages=error_messages, widget=forms.HiddenInput())
-    networkv6 = forms.CharField(label="Rede IPv6", required=False, min_length=1,
-                                max_length=43, error_messages=error_messages, widget=forms.HiddenInput())
-    net_type = forms.ChoiceField(label="Tipo de Rede", required=True, choices=[(
-        0, "Selecione")], error_messages=error_messages, widget=forms.Select(attrs={"style": "width: 180px"}))
-    env_vip = forms.ChoiceField(label="Ambiente VIP", required=False, choices=[(
-        0, "Selecione")], error_messages=error_messages, widget=forms.Select(attrs={"style": "width: 340px"}))
-    cluster_unit = forms.CharField(
-        label="Cluster Unit",
-        required=False,
-        error_messages=error_messages,
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "cluster-unit-1",
-                "style": "width: 180px"
-            }
-        )
-    )
+    vlan_name = forms.CharField(label="Número da Vlan | Ambiente",
+                                min_length=3,
+                                max_length=100,
+                                required=True,
+                                widget=forms.TextInput(attrs={'style': "width: 700px; height: 19px;",
+                                                              'class': "ui-state-default", 'autocomplete': "off"}),
+                                error_messages=error_messages)
+
+    vlan_name_id = forms.IntegerField(label="Id da Vlan",
+                                      required=True,
+                                      error_messages=error_messages,
+                                      widget=forms.HiddenInput())
+
+    ip_version = forms.ChoiceField(label="Rede IPv4/IPv6",
+                                   choices=NETWORK_IP_CHOICES,
+                                   error_messages=error_messages,
+                                   widget=forms.RadioSelect,
+                                   initial=0)
+
+    networkv4 = forms.CharField(label="Rede IPv4",
+                                required=False,
+                                min_length=1,
+                                max_length=18,
+                                error_messages=error_messages,
+                                widget=forms.HiddenInput())
+
+    networkv6 = forms.CharField(label="Rede IPv6",
+                                required=False,
+                                min_length=1,
+                                max_length=43,
+                                error_messages=error_messages,
+                                widget=forms.HiddenInput())
+
+    net_type = forms.ChoiceField(label="Tipo de Rede",
+                                 choices=[(0, "Selecione")],
+                                 error_messages=error_messages,
+                                 widget=forms.Select(attrs={"style": "width: 180px"}))
+
+    env_vip = forms.ChoiceField(label="Ambiente VIP",
+                                required=False, choices=[(0, "Selecione")],
+                                error_messages=error_messages,
+                                widget=forms.Select(attrs={"style": "width: 340px"}))
+
+    cluster_unit = forms.CharField(label="Cluster Unit",
+                                   required=False,
+                                   error_messages=error_messages,
+                                   widget=forms.TextInput(attrs={"placeholder": "cluster-unit-1",
+                                                                 "style": "width: 180px"}))
 
 
 class NetworkEditForm(forms.Form):
+    """Form to edit network
+
+    """
 
     def __init__(self, net_type_list, env_vip_list, *args, **kwargs):
         super(NetworkEditForm, self).__init__(*args, **kwargs)
@@ -109,7 +178,7 @@ class NetworkEditForm(forms.Form):
         required=True,
         widget=forms.TextInput(
             attrs={
-                'style': "width: 300px; height: 19px;",
+                'style': "width: 700px; height: 19px;",
                 'class': "ui-state-default",
                 'autocomplete': "off"
             }
@@ -119,7 +188,6 @@ class NetworkEditForm(forms.Form):
 
     ip_version = forms.ChoiceField(
         label="Rede IPv4/IPv6",
-        required=True,
         choices=NETWORK_IP_CHOICES,
         error_messages=error_messages,
         widget=forms.RadioSelect,
@@ -128,7 +196,6 @@ class NetworkEditForm(forms.Form):
 
     net_type = forms.ChoiceField(
         label="Tipo de Rede",
-        required=True,
         choices=[(
             0, "Selecione"
         )],
