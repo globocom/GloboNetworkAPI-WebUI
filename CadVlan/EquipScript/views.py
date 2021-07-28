918b3ce9d11cfeee6bc526eaf65f54b58c173a55
Luna Belfort <sovietlunox@gmail.com>
2021-07-28 16:24:56 -0300

    Sorted script list and added ID information

--- CadVlan/EquipScript/views.py
+++ CadVlan/EquipScript/views.py
@@ -51,9 +51,11 @@ def search_list(request):
             # Get user
             auth = AuthSession(request.session)
             client = auth.get_clientFactory()
-
+            
             # Get all script_types from NetworkAPI
             script_list = client.create_roteiro().listar()
+            script_list['script'] = sorted(script_list['script'], key=lambda k: k['roteiro'].lower())
+            messages.add_message(request, messages.ERROR, script_list)
 
             lists['add_form'] = AssociateScriptForm(script_list)
 
