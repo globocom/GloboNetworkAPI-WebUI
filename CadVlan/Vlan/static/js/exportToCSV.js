5bed726d02ed77f3426b7943a66f6d9b237fade2
renan.lopes <renan.lopes@g.globo>
2023-03-15 15:49:09 -0300

    added loading

--- CadVlan/Vlan/static/js/exportToCSV.js
+++ CadVlan/Vlan/static/js/exportToCSV.js
@@ -9,6 +9,7 @@ const download = function (text, fileName) {
     link.click();
 
     document.body.removeChild(link);
+    $(".loading").hide()
 };
 
 const startSearch = 0
@@ -54,6 +55,7 @@ function exportToCSV(method, url, fileName) {
     if (!window.confirm("Esta ação pode levar algum tempo, não atualize a página.")){
         return
     }
+    $(".loading").show()
 
     // SUBMIT FORM TO GENERATE A CSV FILE
     let fullUrl = `${url}?csrfmiddlewaretoken=${token}&networkv4=&networkv6=&ip_version=${ipVersion}&number=${number}&name=${name}&environment=${environment}&net_type=${netType}&oct1=${oct1}&oct2=${oct2}&oct3=${oct3}&oct4=${oct4}&oct5=${oct5}&oct6=${oct6}&oct7=${oct7}&oct8=${oct8}&oct9=${oct9}&subnet=${subnet}&iexact=${exact}&sEcho=2&iColumns=8&sColumns=&iDisplayStart=0&iDisplayLength=${entries}`//&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&iSortingCols=0&bSortable_0=false&bSortable_1=false&bSortable_2=true&bSortable_3=true&bSortable_4=true&bSortable_5=true&bSortable_6=true&bSortable_7=false&_=1678200991699`
@@ -65,6 +67,7 @@ function exportToCSV(method, url, fileName) {
             const response = request.response
             let jsonData = response.jsonData
             if (jsonData.length === 0){
+                $(".loading").hide()
                 window.alert("Houve um erro ao exportar os dados. Refaça a busca.")
                 window.location.reload()
                 return
@@ -72,6 +75,7 @@ function exportToCSV(method, url, fileName) {
             let csvData = objectToCSV(jsonData)
             download(csvData, `${fileName}.csv`)
         } else {
+            $(".loading").hide()
             console.log(`Error: ${request.status}`)
         }
     }
