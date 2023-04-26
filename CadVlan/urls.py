e07a8c3820ec273f287d13da17f8e80d5bdcfe60
deivison.marteleto <deivison.marteleto@g.globo>
2023-04-26 14:32:32 -0300

    teste dev

--- CadVlan/urls.py
+++ CadVlan/urls.py
@@ -31,7 +31,7 @@ urlpatterns += patterns(
     '',
     # CSS - JS
     (r'^media/(.*)$', 'django.views.static.serve',
-     {'document_root': settings.MEDIA_ROOT})
+    {'document_root': settings.MEDIA_ROOT})
 )
 
 
